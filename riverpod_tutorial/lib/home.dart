974756e207f01e16f00950c50c5e8546d52662c2
Isaac Hamm <isaachamm3@gmail.com>
2026-07-13 22:21:09 -0600

    Updated home.dart to use joke API dynamically

--- riverpod_tutorial/lib/home.dart
+++ riverpod_tutorial/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_tutorial/joke.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,24 +10,46 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Random Joke Generator')),
       body: SizedBox.expand(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            const SelectableText(
-              'What kind of bagel can fly?\n\n'
-              'A plain bagel.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 24),
-            ),
+        child: Consumer(
+          builder: (context, ref, child) {
+            final randomJoke = ref.watch(randomJokeProvider);
 
-            Positioned(
-              bottom: 20,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Get another joke'),
-              ),
-            ),
-          ],
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                switch (randomJoke) {
+                  // When the request completes successfully, we display the joke.
+                  AsyncValue(:final value?) => SelectableText(
+                    '${value.setup}\n\n${value.punchline}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  // On error, we display a simple error message.
+                  AsyncValue(error: != null) => const Text(
+                    'Error fetching joke',
+                  ),
+                  // While the request is loading, we display a progress indicator.
+                  AsyncValue() => const CircularProgressIndicator(),
+                },
+
+				if (randomJoke.isRefreshing) 
+                	const Positioned(
+						top: 0,
+						left: 0,
+						right: 0,
+						child: LinearProgressIndicator()
+					),
+
+				Positioned(
+                  bottom: 20,
+                  child: ElevatedButton(
+                    onPressed: () => ref.invalidate(randomJokeProvider),
+                    child: const Text('Get another joke'),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
